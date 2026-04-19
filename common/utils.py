abd020cfd18b8e7aadff2f421c675b826e67b80a
ossikoski <ossioskari@gmail.com>
2026-04-19 15:04:18 +0300

    Use only tammer 9r

--- common/utils.py
+++ common/utils.py
@@ -4,12 +4,12 @@ from datetime import datetime
 products = {
     # 'Tammer-golf simulaattori': 683,  # For winter
     'Tammer-golf 9r': 424,
-    'Nokiarivergolf River': 89,
-    'Nokiarivergolf River 9r': 384,
-    'Nokiarivergolf Rock': 88,
-    'Nokiarivergolf Rock 9r': 383,
-    'Golfpirkkala': 42,
-    'Golfpirkkala 9r': 97,
+    # 'Nokiarivergolf River': 89,
+    # 'Nokiarivergolf River 9r': 384,
+    # 'Nokiarivergolf Rock': 88,
+    # 'Nokiarivergolf Rock 9r': 383,
+    # 'Golfpirkkala': 42,
+    # 'Golfpirkkala 9r': 97,
 }
 
 weekdays = {
